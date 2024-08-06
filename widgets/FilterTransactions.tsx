1e29acdbc29f291df533a44a10f34522ba38ed95
Aziul <y.nazar9@gmail.com>
2024-08-06 16:57:15 +0300

    old style of filter UI

--- widgets/FilterTransactions.tsx
+++ widgets/FilterTransactions.tsx
@@ -1,8 +1,9 @@
-import { View } from "react-native"
+import { StyleSheet, View } from "react-native"
 import Search from "../components/transactionSearch/search"
 import { useState } from "react";
 import FilterActionButton from "../components/filter/FilterActionButton";
 import FilterOptions from "../components/filter/FilterOptions";
+import Button from "../components/button/Button";
 
 
 
@@ -17,11 +18,42 @@ const FilterTransactions = () => {
             <FilterOptions visible={showFilters} />
             <View style={{ flexDirection: 'row', alignItems: 'center', gap: 5 }}>
                 <Search onSearch={() => { }} onFocusChange={() => { }} />
-                <FilterActionButton iconName={showFilters ? 'keyboard-arrow-down' : 'keyboard-arrow-up'} title={'фільтри'} onPress={handleActionButtonPress} />
+                <Button
+                    style={[styles.filters]}
+                    variant={'outline'}
+                    size={'sm'}
+                    color="#EDE7F5"
+                    icon={{
+                        name: showFilters ? 'arrow-down' : 'arrow-up',
+                        size: 15,
+                        color: '#512DA8',
+                        containerStyle: {
+                            marginRight: 0
+                        }
+
+                    }}
+                    onPress={handleActionButtonPress}
+                    textStyle={styles.text}
+                >
+                    фільтри
+                </Button>
             </View>
         </View>
 
     )
 }
+
+const styles = StyleSheet.create({
+    filters: {
+        flex: 1,
+        borderColor: "rgba(104, 58, 183, 0.4)",
+        marginRight: 5,
+    },
+    text: {
+        fontSize: 15,
+        color: '#512DA8',
+    }
+});
+
 
 export default FilterTransactions
