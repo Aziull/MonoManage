4d10e2c07601f1b21cb4ac1fc930886d4ad642f5
Aziul <y.nazar9@gmail.com>
2024-08-16 01:32:35 +0300

    editing

--- components/dateInput/SingleDateInput.tsx
+++ components/dateInput/SingleDateInput.tsx
@@ -1,14 +1,15 @@
 import { useState } from "react";
-import Button from "../Button";
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import DatePickerModal, { Single } from "../../modal/DatePickerModal";
 import dayjs, { Dayjs } from "dayjs";
 import { StyleSheet, Text } from "react-native";
+import Button from "../button/Button";
 type Props = {
-    date: Dayjs
+    date: Date
     setDate: (date: Dayjs) => void
 }
-const SingleDateInput = ({ setDate, date }: Props) => {
+const SingleDateInput = ({ setDate, date: dateDate }: Props) => {
+    const date = dayjs(dateDate)
     const [isDatePickerVisible, setDatePickerVisible] = useState(false);
     const onSubmit = (data: Single) => {
         setDate(dayjs(data.date));
@@ -16,16 +17,20 @@ const SingleDateInput = ({ setDate, date }: Props) => {
     };
     return (
         <>
-            <Button style={styles.dateButton} containerStyle={{
+            <Button variant='ghost' style={styles.dateButton} containerStyle={{
                 flexDirection: 'row',
                 alignItems: 'center',
                 columnGap: 3
             }}
                 onPress={() => setDatePickerVisible(true)}
+                icon={{
+                    name: 'calendar',
+                    size: 18,
+                    color: 'rgba(82, 45, 168, 0.7)'
+                }}
             >
-                <MaterialIcons name='date-range' size={15} color='rgba(82, 45, 168, 0.7)' />
                 <Text style={styles.dateButtonText}>
-                    {date?.format ? date.format("DD MMMM YYYY") : ""}
+                    {new Intl.DateTimeFormat('uk-UA', { day: 'numeric', month: 'long', year: 'numeric' }).format(dateDate)}
                 </Text>
             </Button>
 
@@ -51,7 +56,7 @@ const SingleDateInput = ({ setDate, date }: Props) => {
 
 const styles = StyleSheet.create({
     dateButton: {
-        padding: 10,
+        marginHorizontal: 'auto',
     },
     dateButtonText: {
         color: 'rgba(82, 45, 168, 0.7)', // Фіолетовий колір тексту
