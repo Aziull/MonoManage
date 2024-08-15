df2ac013a57c529874a369e7411c9d9fa990920d
Aziul <y.nazar9@gmail.com>
2024-08-16 01:33:43 +0300

    edit transactions search

--- components/transactionSearch/search.tsx
+++ components/transactionSearch/search.tsx
@@ -1,5 +1,5 @@
-import React, { ChangeEvent, useEffect, useRef, useState } from 'react';
-import { TouchableWithoutFeedback } from 'react-native';
+import React, { ChangeEvent, useCallback, useEffect, useRef, useState } from 'react';
+import { TouchableWithoutFeedback, ViewStyle } from 'react-native';
 import { StyleSheet, View, TextInput, Keyboard, TouchableOpacity, KeyboardAvoidingView, Platform } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import { useDispatch, useSelector } from 'react-redux';
@@ -8,23 +8,17 @@ import { setDescription } from '../../features/filter/slice';
 import Search from '../Search';
 
 type SearchProps = {
-    placeholder?: string;
-    onSearch: (query: string) => void; // Функція, яка викликається з пошуковим запитом
-    onFocusChange: Function;
+    style?: ViewStyle
 };
 
-const TransactionSearch: React.FC<SearchProps> = ({ placeholder = "Пошук", onSearch, onFocusChange }) => {
+const TransactionSearch: React.FC<SearchProps> = ({ style }) => {
     const description = useSelector((state: RootState) => state.filters.description);
     const dispatch = useDispatch();
-
-    const handleDescriptionChange = (value: string) => {
+    const handleDescriptionChange = useCallback((value: string) => {
         dispatch(setDescription(value));
-    };
+    }, [dispatch]);
 
     const inputRef = useRef<TextInput>(null)
-    const handleSearch = () => {
-        inputRef?.current?.focus();
-    };
 
     useEffect(() => {
         const hide = Keyboard.addListener('keyboardDidHide', () => {
@@ -38,42 +32,12 @@ const TransactionSearch: React.FC<SearchProps> = ({ placeholder = "Пошук", 
 
     return (
         <Search
+            style={style}
             value={description}
             handleValueChange={handleDescriptionChange}
-            handleSearch={handleSearch}
             ref={inputRef}
         />
     );
 };
-
-const styles = StyleSheet.create({
-    container: {
-        alignSelf: 'flex-start',
-        flexDirection: 'row',
-        alignItems: 'center',
-        backgroundColor: '#fff',
-        width: "50%",
-        margin: 5,
-        borderRadius: 10,
-        borderWidth: 1,
-        borderColor: "#512DA8",
-        paddingHorizontal: 5
-    },
-    iconContainer: {
-        paddingRight: 5,
-    },
-    icon: {
-        color: "#512DA8",
-    },
-    input: {
-        width: '80%',
-        paddingRight: 15,
-    },
-    closeContainer: {
-        position: "relative",
-        left: -15
-
-    },
-});
 
 export default TransactionSearch;
